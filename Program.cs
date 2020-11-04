626506968f680d524b618ae08518f4fd9dbf93a8
Odin <64448868+O-D-1-N@users.noreply.github.com>
2020-11-04 23:50:09 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+
+using VideoLibrary;
+
+namespace youtube_dl {
+    class Program {
+        static void Main() {
+
+            List<string> args = new List<string>(Environment.GetCommandLineArgs());
+            args.RemoveAt(0);
+
+            try {
+                string ytlink = args[0];
+
+                if (ytlink.StartsWith("https://www.youtube.com/watch?v=")) {
+
+                    Info("Starting YouTube Player...");
+                    var youtube = YouTube.Default;
+                    var video = youtube.GetVideo(ytlink);
+
+                    Info($"Video name = [\"{video.FullName}\"]");
+                    string dir = "D:\\youtube-dl";
+                    File.WriteAllBytes($"{dir}\\{video.FullName}", video.GetBytes());
+
+                    Info($"Video Downloaded successfully");
+                
+                } else {
+                    Error("Invalid YouTube link.");
+                }
+
+            }
+
+            catch (System.ArgumentOutOfRangeException e) {
+                Error("No YouTube link provided.");
+            }
+
+            catch(Exception e) {
+                Error(e.Message);
+            }
+
+        }
+
+        static void Info(string info) {
+            Console.WriteLine($"[INFO]: {info}");
+        }
+
+        static void Error(string error) {
+            Console.WriteLine($"[ERROR]: {error}");
+        }
+
+    }
+}
